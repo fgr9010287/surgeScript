df207f6854ad8fd5694f93a011292e8d46942115
GitHub <fgr9010287@gmail.com>
2022-11-14 00:30:05 +0800

    dayone.js
    添加pdf

--- resources/script/dayone.js
+++ resources/script/dayone.js
@@ -18,9 +18,8 @@ MitM = dayone.me
 */
 
 if ($response.status == 200) {
-        if ($request.url.endsWith("account-status")) {
-
-            $done({body: JSON.stringify({
+  if ($request.url.endsWith("account-status")) {
+$done({body: JSON.stringify({
   "expirationDate": 1893427199000,
   "startDate": 1546272000000,
   "subscriptionPlan": "com.bloombuilt.dayoneios.subscription.premium.yearly_discounted_trial",
@@ -29,13 +28,18 @@ if ($response.status == 200) {
   "bundleReason": "purchase",
   "cancellationDate": 0
 } )});
-
-        } else {
-	     const bodyJson = JSON.parse($response.body);
-
-	     bodyJson.featureBundle = {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false}]};
-
-	    $done({body: JSON.stringify(bodyJson)});
+} 
+else { let body= $response.body;
+	     let obj= JSON.parse(body);
+	     const feature= {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false},{"name":"scanToPDF","limit":null,"canUpgrade":false}]};
+    if(body.indexOf("featureBundle") !=-1)
+      {
+      obj["featureBundle"]= feature;
+      }
+       else {
+	    	obj["bundle"]= feature;
+	    }
+	    $done({body: JSON.stringify(obj)});
         }
 } else {
 	$done({});
